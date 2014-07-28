f6f2d1ab2f7257c12e8bb6c3dfd185699d8467b5
a5mith <aaronsmith91@sky.com>
2014-07-28 20:11:54 +0100

    testing

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ var	request = require('request'),
     Embed = {},
     cache, appModule;
 
+var getDiscog = function(discogsKey, callback) {
+    var discogNum = discogsKey.split('#')[1];
+    console.log('getting discog info', discogNum);
+
+    request.get({
+        url: 'http://api.discogs.com/database/search?catno=' + discogNum + '&type=release&per_page=1',
+        headers: {
+            'accept-encoding': 'gzip',
+            'User-Agent': '35hzMusicDiscogs/1.0 +http://35hz.co.uk'
+        },
+        encoding: null
+
+    }, function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+            callback(null, body),
+                console.log(body)
+        } else {
+            console.log(err, response);
+        }
+    });
+};
+
 Embed.init = function(app, middleware, controllers, callback) {
     function render(req, res, next) {
         res.render('partials/discog-block', {});
@@ -61,38 +83,6 @@ Embed.parse = function(raw, callback) {
         } else {
             winston.warn('Encountered an error parsing discog embed code, not continuing');
             callback(null, raw);
-        }
-    });
-};
-
-var getDiscog = function(discogsKey, callback) {
-    var discogNum = discogsKey.split('#')[1];
-    console.log('getting discog info', discogNum);
-
-    request.get({
-        url: 'http://api.discogs.com/database/search?catno=' + discogNum + '&type=release&per_page=1',
-        headers: {
-            'accept-encoding': 'gzip',
-            'User-Agent': '35hzMusicDiscogs/1.0 +http://35hz.co.uk'
-        },
-        encoding: null
-
-    }, function(err, response, body) {
-        if (!err && response.statusCode === 200) {
-            callback(null, body),
-                returnData = {
-                    results: {
-                        uri: results[0].uri,
-                        thumbnail: results[0].thumb,
-                        catno: results[0].catno,
-                        title: results[0].title,
-                        style: results[0].style,
-                        label: results[0].label,
-                        year: results[0].year
-                    }
-                };
-        } else {
-            console.log(err, response);
         }
     });
 };
