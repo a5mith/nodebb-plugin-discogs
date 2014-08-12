579186035c9db7911ab230044a2bcdea5f4a32fa
Julian Lam <julian@designcreateplay.com>
2014-08-12 09:46:06 -0400

    updated styling and fixed template implementation

--- index.js
+++ index.js
@@ -1,109 +1,109 @@
-var	request = require('request'),
-    async = module.parent.require('async'),
-    winston = module.parent.require('winston'),
-    S = module.parent.require('string'),
-    meta = module.parent.require('./meta'),
+(function() {
+    "use strict";
+    /* jshint indent: 4 */
 
-    discogsRegex = /dc#\w+/gm,
-    Embed = {},
-    cache, appModule;
+    var	request = require('request'),
+        async = module.parent.require('async'),
+        winston = module.parent.require('winston'),
+        S = module.parent.require('string'),
+        meta = module.parent.require('./meta'),
 
-var getDiscog = function(discogsKey, callback) {
-    var discogNum = discogsKey.split('#')[1];
-    console.log('getting discog info', discogNum);
+        discogsRegex = /dc#\w+/gm,
+        Embed = {},
+        cache, appModule;
 
-    request.get({
-        url: 'http://api.discogs.com/database/search?catno=' + discogNum + '&type=release&per_page=1',
-        headers: {
-            'User-Agent': '35hzMusicDiscogs/1.0 +http://35hz.co.uk'
-        },
-        dataType: 'json'
-    },
+    var getDiscog = function(discogsKey, callback) {
+        var discogNum = discogsKey.split('#')[1];
+        winston.info('[discogs] Retrieving info: ' + discogNum);
 
-        function (err, response, body) {
-        if (!err && response.statusCode === 200) {
-            console.log('connected...')
-                var results = (body),
-                    returnData = {
-                        results: [{
-                            uri: results[0].uri,
-                            thumbnail: results[0].thumb,
-                            catno: results[0].catno,
-                            title: results[0].title,
-                            style: results[0].style,
-                            label: results[0].label,
-                            year: results[0].year
-                        }]
-                    };
-                console.log(results)
-            callback(null, results);
-        } else {
-            console.log(err, response);
-        }
-    });
-};
+        request.get({
+            url: 'http://api.discogs.com/database/search?catno=' + discogNum + '&type=release&per_page=1',
+            headers: {
+                'User-Agent': '35hzMusicDiscogs/1.0 +http://35hz.co.uk'
+            },
+            dataType: 'json'
+        }, function (err, response, body) {
+            if (!err && response.statusCode === 200) {
+                var albumData = JSON.parse(body).results[0];
 
-Embed.init = function(app, middleware, controllers, callback) {
-    function render(req, res, next) {
-        res.render('partials/discog-block', {});
-    }
-
-    appModule = app;
-
-    callback();
-};
-
-Embed.parse = function(raw, callback) {
-    var discogsKeys = [],
-        matches, cleanedText;
-
-    cleanedText = S(raw).stripTags().s;
-    matches = cleanedText.match(discogsRegex);
-
-    if (matches && matches.length) {
-        matches.forEach(function(match) {
-            if (discogsKeys.indexOf(match) === -1) {
-                discogsKeys.push(match);
+                console.log(albumData);
+                callback(null, {
+                    uri: albumData.uri,
+                    thumbnail: albumData.thumb,
+                    catno: albumData.catno,
+                    title: albumData.title,
+                    style: Array.isArray(albumData.style) ? albumData.style[0] : albumData.style,
+                    label: Array.isArray(albumData.label) ? albumData.label[0] : albumData.label,
+                    year: albumData.year
+                });
+            } else {
+                console.log(err, response);
             }
         });
-    }
+    };
 
-    async.map(discogsKeys, function(discogsKey, next) {
-        if (cache.has(discogsKey)) {
-            next(null, cache.get(discogsKey));
-        } else {
-            getDiscog(discogsKey, function(err, discogsObj) {
-                if (err) {
-                    return next(err);
+    Embed.init = function(app, middleware, controllers, callback) {
+        function render(req, res, next) {
+            res.render('partials/discog-block', {});
+        }
+
+        appModule = app;
+
+        callback();
+    };
+
+    Embed.parse = function(raw, callback) {
+        var discogsKeys = [],
+            matches, cleanedText;
+
+        cleanedText = S(raw).stripTags().s;
+        matches = cleanedText.match(discogsRegex);
+
+        if (matches && matches.length) {
+            matches.forEach(function(match) {
+                if (discogsKeys.indexOf(match) === -1) {
+                    discogsKeys.push(match);
                 }
-
-                cache.set(discogsKey, discogsObj);
-                next(err, discogsObj);
             });
         }
-    }, function(err, discoginfo) {
-        if (!err) {
-            // Filter
-            discoginfo = discoginfo.filter(function(issue) {
-                return issue;
-            });
 
-            appModule.render('partials/discog-block', {
-                discoginfo: discoginfo
-            }, function(err, html) {
-                callback(null, raw += html);
-            });
-        } else {
-            winston.warn('Encountered an error parsing discog embed code, not continuing');
-            callback(null, raw);
-        }
+        async.map(discogsKeys, function(discogsKey, next) {
+            if (cache.has(discogsKey)) {
+                next(null, cache.get(discogsKey));
+            } else {
+                getDiscog(discogsKey, function(err, discogsObj) {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    cache.set(discogsKey, discogsObj);
+                    next(err, discogsObj);
+                });
+            }
+        }, function(err, discoginfo) {
+            if (!err) {
+                // Filter
+                discoginfo = discoginfo.filter(function(issue) {
+                    return issue;
+                });
+
+                appModule.render('partials/discog-block', {
+                    discoginfo: discoginfo
+                }, function(err, html) {
+                    callback(null, raw += html);
+                });
+            } else {
+                winston.warn('Encountered an error parsing discog embed code, not continuing');
+                callback(null, raw);
+            }
+        });
+    };
+
+    // Initial setup
+    cache = require('lru-cache')({
+        maxAge: 1000*60*60*24,
+        max: 100
     });
-};
 
-// Initial setup
-cache = require('lru-cache')({
-    maxAge: 1000*60*60*24,
-    max: 100
-});
-
-module.exports = Embed;
+    module.exports = Embed;
+})();
