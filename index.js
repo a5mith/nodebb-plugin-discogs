b3602b654105d66d05bf580a9bef2b7507e457cf
a5mith <aaronsmith91@sky.com>
2014-08-12 15:29:47 +0100

    0.1

--- index.js
+++ index.js
@@ -99,6 +99,14 @@
         });
     };
 
+    var imageUrl = {},
+        imageUrl = 'http://s.pixogs.com/image/R-150-$1';
+
+    imageUrl.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:api\.discogs\.com)\/image\/R\-90\-?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
     // Initial setup
     cache = require('lru-cache')({
         maxAge: 1000*60*60*24,
